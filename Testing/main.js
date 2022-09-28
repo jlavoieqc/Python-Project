bdd877c6f5a6629f1c3fc82dd78a56cb539e96d0
Justin <jlavoieqc@gmail.com>
2022-09-28 17:36:29 -0400

    Pushing to a remote repository

--- Testing/main.js
+++ Testing/main.js
@@ -1,110 +1,17 @@
-var board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ];
-  
-  var player1 = 'X';
-  var player2 = 'O';
-  
-  var currentPlayer = player1;
-  
-  function play(row, col) {
-    if (board[row][col] === ' ') {
-      board[row][col] = currentPlayer;
-  
-      if (currentPlayer === player1) {
-        currentPlayer = player2;
-      } else {
-        currentPlayer = player1;
-      }
-  
-      renderBoard();
-  
-      checkForWinner();
-    } else {
-      alert('That space is already taken!');
-    }
-  }
-  
-  function renderBoard() {
-    for (var i = 0; i < board.length; i++) {
-      console.log(board[i].join(' | '));
-    }
-  
-    console.log('\n\n');
-  }
-  
-  function checkForWinner() {
-    // check rows
-  
-    for (var i = 0; i < board.length; i++) {
-      if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-        alert(currentPlayer + ' wins!');
-  
-        resetBoard();
-  
-        return; // stop execution of this function! we found a winner!
-      }
-    }
-  
-    // check cols
-  
-    for (var j = 0; j < board.length; j++) {
-      if (board[0][j] === board[1][j] && board[1][j] === board[2][j]) {
-        alert(currentPlayer + ' wins!');
-  
-        resetBoard();
-  
-        return; // stop execution of this function! we found a winner!
-      }
-    }
-  
-    // check diags
-  
-    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-      alert(currentPlayer + ' wins!');
-  
-      resetBoard();
-  
-      return; // stop execution of this function! we found a winner!
-    } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-      alert(currentPlayer + ' wins!');
-  
-      resetBoard();
-  
-      return; // stop execution of this function! we found a winner!    
-    } else if (isBoardFull()) { // check for tie game! all spaces filled and no winner yet...
-      alert('Tie game!');
-  
-      resetBoard();
-  
-      return; // stop execution of this function! we have a tie game!    
-    } else { // no winner yet... keep playing!    
-      return; // stop execution of this function! keep playing!    
-    }  
-  }
-  
-  function isBoardFull() {
-    for (var i = 0; i < board.length; i++) {
-      for (var j = 0; j < board[i].length; j++) {
-        if (board[i][j] === ' ') {
-          return false; // we found an empty space! keep playing!
-        }
-      }
-    }
-  
-    return true; // all spaces are full! it's a tie game!
-  }
-  
-  function resetBoard() {
-    board = [
-      [' ', ' ', ' '],
-      [' ', ' ', ' '],
-      [' ', ' ', ' ']
-    ];
-  
-    currentPlayer = player1;
-  
-    renderBoard();
-  }
+var dinosaurs = ["T-Rex", "Brachiosaurus", "Pterodactyl", "Stegosaurus", "Triceratops"];
+
+var userChoice = prompt("Which dinosaur would you like to learn about? We have T-Rex, Brachiosaurus, Pterodactyl, Stegosaurus, and Triceratops.");
+
+if (userChoice === "T-Rex") {
+  console.log("The T-Rex is one of the most popular dinosaurs. It lived during the Cretaceous period and was one of the last non-avian dinosaurs to exist. It was a carnivore and could grow up to 40 feet long and weigh up to 7 tons.");
+} else if (userChoice === "Brachiosaurus") {
+  console.log("The Brachiosaurus was a large sauropod that lived during the Jurassic period. It was one of the largest land animals ever to exist, with some estimates suggesting it could grow up to 85 feet long and weigh up to 80 tons.");
+} else if (userChoice === "Pterodactyl") {
+  console.log("The Pterodactyl was a flying reptile that lived during the Jurassic and Cretaceous periods. It was the largest known flying animal of all time, with some estimates suggesting it could have a wingspan of up to 40 feet.");
+} else if (userChoice === "Stegosaurus") {
+  console.log("The Stegosaurus was a large herbivorous dinosaur that lived during the Jurassic period. It was easily recognizable due to the large plates on its back, which were used for defense against predators.");
+} else if (userChoice === "Triceratops") {
+  console.log("The Triceratops was a large herbivorous dinosaur that lived during the Cretaceous period. It was easily recognizable due to its three horns and large frill.");
+} else {
+  console.log("That is not a valid dinosaur. Please try again.");
+}
