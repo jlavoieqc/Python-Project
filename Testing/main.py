aa6ba95028bde460627c5fa829255c1b625cda06
jlavoieqc <97467718+jlavoieqc@users.noreply.github.com>
2022-09-30 12:36:05 -0400

    Push

--- Testing/main.py
+++ Testing/main.py
@@ -1,65 +1,65 @@
+import os
+import time
 import random
-import urllib.request
+import argparse
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-#here is the list of books that will be generated
-#url
-movies_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/movies.txt"
-books_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/books.txt"
-tv_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/tv.txt"
-vg_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/vg.txt"
-music_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/music.txt"
-food_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/food.txt"
-animals_url = "https://raw.githubusercontent.com/jlavoieqc/Python-Project/main/Topic%20Generator/animals.txt"
 
 def main():
-    #movies
-    response = urllib.request.urlopen(movies_url)
-    long_txt = response.read().decode()
-    movies_list = long_txt.splitlines()
-    #books
-    response = urllib.request.urlopen(books_url)
-    long_txt = response.read().decode()
-    books_list = long_txt.splitlines()
-    #tv
-    response = urllib.request.urlopen(tv_url)
-    long_txt = response.read().decode()
-    tv_list = long_txt.splitlines()
-    #vg
-    response = urllib.request.urlopen(vg_url)
-    long_txt = response.read().decode()
-    vg_list = long_txt.splitlines()
-    #music
-    response = urllib.request.urlopen(music_url)
-    long_txt = response.read().decode()
-    music_list = long_txt.splitlines()
-    #food
-    response = urllib.request.urlopen(food_url)
-    long_txt = response.read().decode()
-    food_list = long_txt.splitlines()
-    #animals
-    response = urllib.request.urlopen(animals_url)
-    long_txt = response.read().decode()
-    animals_list = long_txt.splitlines()
-    
-    
-    #choose a category
-    category = input('Choisit Une Catégorie: (1) Films, (2) Livres, (3) TV Shows, (4) Jeux Vidéos, (5) Musique, (6) Sport, (7) Nourriture, (8) Animaux, (9) Pays: ')
-    #choose a word
-    if category == '1':
-        print(random.choice(movies_list))
-    elif category == '2':
-        print(random.choice(books_list))
-    elif category == '3':
-        print(random.choice(tv_list))
-    elif category == '4':
-        print(random.choice(vg_list))
-    elif category == '5':
-        print(random.choice(music_list))
-    elif category == '6':
-        print(random.choice(food_list))
-    elif category == '7':
-        print(random.choice(animals_list))
-    else:
-        print('Choisit une catégorie valide')
-        main()
-main()
+    """Main function"""
+
+    # Arguments
+    parser = argparse.ArgumentParser(description='Google hangout spammer')
+    parser.add_argument('-u', '--username', required=True, help='Your Google username')
+    parser.add_argument('-p', '--password', required=True, help='Your Google password')
+    parser.add_argument('-t', '--target', required=True, help='The target email address')
+    parser.add_argument('-m', '--message', required=True, help='The message to spam')
+    parser.add_argument('-i', '--interval', required=False, default=1, type=int, help='The interval between messages')
+    parser.add_argument('-c', '--count', required=False, default=1, type=int, help='The number of messages to send')
+    args = parser.parse_args()
+
+    # Start the browser
+    driver = webdriver.Chrome()
+    driver.get('https://hangouts.google.com/')
+
+    # Wait for the login page to load
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'Email')))
+
+    # Login
+    driver.find_element_by_id('Email').send_keys(args.username)
+    driver.find_element_by_id('next').click()
+    wait.until(EC.presence_of_element_located((By.ID, 'Passwd')))
+    driver.find_element_by_id('Passwd').send_keys(args.password)
+    driver.find_element_by_id('signIn').click()
+
+    # Wait for the main page to load
+    wait.until(EC.presence_of_element_located((By.ID, 'gbwa')))
+
+    # Start a new conversation
+    driver.find_element_by_id('gbwa').click()
+    driver.find_element_by_id('gb23').click()
+    wait.until(EC.presence_of_element_located((By.ID, 'gbqfq')))
+    driver.find_element_by_id('gbqfq').send_keys(args.target)
+    driver.find_element_by_id('gbqfb').click()
+
+    # Wait for the conversation to load
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'vN')))
+
+    # Spam the message
+    for i in range(args.count):
+        driver.find_element_by_class_name('vN').send_keys(args.message)
+        driver.find_element_by_class_name('vO').click()
+        time.sleep(args.interval)
+
+    # Close the browser
+    driver.close()
+
+
+if __name__ == '__main__':
+    main()
