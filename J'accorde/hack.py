c2a0efe13b1361351e233d0ff0e122229602c8f7
jlavoieqc <97467718+jlavoieqc@users.noreply.github.com>
2022-10-12 15:56:23 -0400

    push

---
+++ J'accorde/hack.py
@@ -0,0 +1,20 @@
+fetch("https://jaccorde.com/app/api/history", {
+  "headers": {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-CA,en;q=0.9,fr-CA;q=0.8,fr;q=0.7,en-US;q=0.6",
+    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvamFjY29yZGUuY29tIiwiaWF0IjoxNjY1NjAyNDk3LCJleHAiOjE2NjU2MDM2OTcsInVzZXIiOjEyMDA2Nywicm9sZSI6W3sidHlwIjoiRyIsImV4cCI6MTY5MDg2MjQwMCwiZ3JpIjo1MjMyLCJncm4iOiJHcm91cGUgMjIyIiwicm9sIjoyfV19.xE9g9AepvbsOq-w9wZfaWcHdM8J-dBx7_7LCPCdTI7Q",
+    "content-type": "application/json",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Chrome OS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest",
+    "cookie": "cookie-storage=1; _ga=GA1.2.703946503.1664221191; refreshToken=xQBHb8f2XVd06KqXPi5Bz9B2S; __utma=246220831.703946503.1664221191.1664475939.1665602435.4; __utmc=246220831; __utmz=246220831.1665602435.4.4.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; __utmb=246220831.2.10.1665602435; _gid=GA1.2.512660292.1665602497",
+    "Referer": "https://jaccorde.com/app/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"topic\":\"classes-de-mots\",\"category\":\"pronoms\",\"exercise\":501,\"date\":\"2022-10-12 15:21:40\",\"duration\":167081,\"score\":0,\"passed\":false,\"data\":[{\"fragmentId\":1,\"questionId\":1,\"correctAnswerId\":[17],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":2,\"correctAnswerId\":[24],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":3,\"correctAnswerId\":[25],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":4,\"correctAnswerId\":[26],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":5,\"correctAnswerId\":[30],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":6,\"correctAnswerId\":[44],\"type\":\"all\"},{\"fragmentId\":1,\"questionId\":7,\"correctAnswerId\":[45],\"type\":\"all\"},{\"fragmentId\":2,\"questionId\":1,\"correctAnswerId\":[13],\"type\":\"all\"},{\"fragmentId\":2,\"questionId\":2,\"correctAnswerId\":[14],\"type\":\"all\"},{\"fragmentId\":2,\"questionId\":3,\"correctAnswerId\":[24],\"type\":\"all\"},{\"fragmentId\":2,\"questionId\":4,\"correctAnswerId\":[29],\"type\":\"all\"},{\"fragmentId\":3,\"questionId\":1,\"correctAnswerId\":[11],\"type\":\"all\"},{\"fragmentId\":3,\"questionId\":2,\"correctAnswerId\":[28],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":1,\"correctAnswerId\":[1],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":2,\"correctAnswerId\":[19],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":3,\"correctAnswerId\":[28,29],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":4,\"correctAnswerId\":[31],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":5,\"correctAnswerId\":[32],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":6,\"correctAnswerId\":[33],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":7,\"correctAnswerId\":[53],\"type\":\"all\"}],\"assignments\":[1963808],\"invalids\":[{\"fragmentId\":1,\"questionId\":0,\"correctAnswerId\":[null],\"type\":\"all\"},{\"fragmentId\":2,\"questionId\":0,\"correctAnswerId\":[null],\"type\":\"all\"},{\"fragmentId\":3,\"questionId\":0,\"correctAnswerId\":[null],\"type\":\"all\"},{\"fragmentId\":4,\"questionId\":0,\"correctAnswerId\":[null],\"type\":\"all\"}],\"user\":\"120067\",\"name\":\"history entry #c252\"}",
+  "method": "POST"
+});
